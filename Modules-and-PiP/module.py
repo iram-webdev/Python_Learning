4fc96ea085c15ed428757a9b50d6709a17931be1
Iram Parween <iram.webdev@gmail.com>
2026-09-16 22:58:39 +0530

    Initial Commit

---
+++ Modules-and-PiP/module.py
@@ -0,0 +1,8 @@
+import pandas  # This is external module
+print("pandas is working")
+
+import requests 
+print("requests is working")
+
+import math    # this is a built in module
+print(math.sqrt(16))
--- hello.py
+++ hello.py
@@ -1,11 +1,2 @@
-print("iram")
-
-var=(10,20,"iram",40)
-print(type(var))
-
-Var=["iram",50,40,40]
-print(type(Var))
-
-
-
-
+import pandas
+print("pandas is working")
