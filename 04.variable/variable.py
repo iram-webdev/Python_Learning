39854353ed96b19ad1c1f063d4d2028a2aef6c65
Iram Parween <iram.webdev@gmail.com>
2026-09-26 11:23:38 +0530

    Initial Commit

--- 04.variable/variable.py
+++ 04.variable/variable.py
@@ -3,7 +3,19 @@
 name = "Iram"
 age = 20
 course = "B.Tech CSE"
-
+z= 3 + 2j
+my_frozen= frozenset(())
+my_set={9,0,7,5}
+my_set.add(2)
+my_set.add(0) # duplication is no allowed
 print(name)
 print(age)
 print(course)
+print(z)
+
+#exemple of Datatype
+
+print(type(name))
+print(type(z))
+print(type(my_set))
+print(my_set)
